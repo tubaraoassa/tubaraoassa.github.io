b6d401db308de62fc3a041974c6a14e311a513b2
tubaraoassa <69279565+tubaraoassa@users.noreply.github.com>
2021-11-19 17:00:18 -0300

    dever casa aula 12 incompleta

--- aula12/cep/js/cep.js
+++ aula12/cep/js/cep.js
@@ -5,6 +5,8 @@ const INPUT_BAIRRO = document.getElementById('bairro');
 const INPUT_CIDADE = document.getElementById('cidade');
 const INPUT_UF = document.getElementById('uf');
 
+//chamar API correios
+
 INPUT_CEP.addEventListener('blur', () => {
   if (INPUT_CEP.value.length < 8) {
     return;
@@ -23,4 +25,72 @@ fetch(`https://viacep.com.br/ws/${INPUT_CEP.value}/json`)
     INPUT_UF.value = dados.uf;
     INPUT_NUMERO.focus();
   });
+});
+
+//validacao
+
+INPUT_CEP.addEventListener('keyup', function () {
+  INPUT_CEP.classList.remove('is-invalid');
+
+  if (INPUT_CEP.value.length === 8) {
+    INPUT_CEP.classList.add('is-valid');
+  }
+  else {
+    INPUT_CEP.classList.add('is-invalid');
+  }
+});
+
+INPUT_LOGRADOURO.addEventListener('keyup', function (){
+  INPUT_LOGRADOURO.classList.remove('is-invalid');
+
+  if (INPUT_LOGRADOURO.value.length >= 10) {
+    INPUT_LOGRADOURO.classList.add('is-valid');
+  }
+  else {
+    INPUT_LOGRADOURO.classList.add('is-invalid');
+  }
+});
+
+INPUT_NUMERO.addEventListener('keyup', function (){
+  INPUT_NUMERO.classList.remove('is-invalid');
+
+if (INPUT_NUMERO.value >= 0) {
+  INPUT_NUMERO.classList.add('is-valid');
+}
+else {
+  INPUT_NUMERO.classList.add('is-invalid');
+}
+});
+
+INPUT_BAIRRO.addEventListener('keyup', function (){
+  INPUT_BAIRRO.classList.remove('is-invalid');
+
+  if (INPUT_BAIRRO.value.length >= 1) {
+    INPUT_BAIRRO.classList.add('is-valid');
+  }
+  else {
+    INPUT_BAIRRO.classList.add('is-invalid');
+  }
+});
+
+INPUT_CIDADE.addEventListener('keyup', function (){
+  INPUT_CIDADE.classList.remove('is-invalid');
+
+  if (INPUT_CIDADE.value.length >= 1) {
+    INPUT_CIDADE.classList.add('is-valid');
+  }
+  else {
+    INPUT_CIDADE.classList.add('is-invalid');
+  }
+});
+
+INPUT_UF.addEventListener('keyup', function (){
+  INPUT_UF.classList.remove('is-invalid');
+
+  if (INPUT_UF.value.length >= 2) {
+    INPUT_UF.classList.add('is-valid');
+  }
+  else {
+    INPUT_UF.classList.add('is-invalid');
+  }
 });
