3f7b4a822929ec40ce020a67f1f70f83649cad25
tubaraoassa <69279565+tubaraoassa@users.noreply.github.com>
2021-11-22 15:33:46 -0300

    dever de casa aula13

--- aula12/ibge/js/buscar.js
+++ aula12/ibge/js/buscar.js
@@ -5,12 +5,15 @@ const urlCidades = (uf) => `https://servicodados.ibge.gov.br/api/v1/localidades/
 const SELECT_REGIAO = document.getElementById('regiao');
 const SELECT_ESTADO = document.getElementById('estado');
 const SELECT_CIDADE = document.getElementById('cidade');
+const IMAGEM_LOADING1 = document.getElementById('loading1');
+const IMAGEM_LOADING2 = document.getElementById('loading2');
 
+//select regiao
 fetch(URL_REGIAO)
   .then((resposta) => resposta.json())
   .then((regioes) => {
     regioes.forEach((cadaRegiao) => {
-      SELECT_REGIAO.innerHTML += `<option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>`;
+      SELECT_REGIAO.innerHTML += `<option value=${cadaRegiao.id}>${cadaRegiao.nome}</option>`;
     });
   })
   .catch((error) => {
@@ -19,30 +22,39 @@ fetch(URL_REGIAO)
 
 //regiao pra estado
 SELECT_REGIAO.addEventListener('change', () => {
+  IMAGEM_LOADING1.src = 'img/loading.gif'
+  setTimeout(() =>{IMAGEM_LOADING1.style.display = "none"}, 3000);
   let regiaoId = SELECT_REGIAO.value;
 
-  SELECT_ESTADO.innerHTML = `<option> --Selecione -- </option>`;
+  SELECT_ESTADO.innerHTML = `<option>-- Selecione --</option>`;
 
   fetch(urlEstados(regiaoId))
     .then((resposta) => resposta.json())
     .then((estados) => {
       estados.forEach((cadaEstado) => {
-        SELECT_ESTADO.innerHTML += `<option value="${cadaEstado.id}">${cadaEstado.nome}</option>`;
+        SELECT_ESTADO.innerHTML += `<option value=${cadaEstado.id}>${cadaEstado.nome}</option>`;
+        IMAGEM_LOADING1.style.display = "none"
       });
     });
 });
 
 //estado pra cidades
 SELECT_ESTADO.addEventListener('change', () => {
-  let estadoId = SELECT_ESTADO.value;
+IMAGEM_LOADING2.src = 'img/loading.gif'
+setTimeout(() =>{IMAGEM_LOADING2.style.display = "none"}, 3000);
 
+
+  let estadoId = SELECT_ESTADO.value;
   SELECT_CIDADE.innerHTML = `<option>-- Selecione --</option>`;
 
-  fetch(urlCidades(estadoId))
-    .then(response => response.json())
-    .then(cidades => {
-      cidades.forEach(cadaCidade => {
-        SELECT_CIDADE.innerHTML += `<option>${cadaCidade.nome}</option>`;
-      })
-    });
+    fetch(urlCidades(estadoId))
+      .then(response => response.json())
+      .then(cidades => {
+        cidades.forEach(cadaCidade => {
+          SELECT_CIDADE.innerHTML += `<option>${cadaCidade.nome}</option>`;
+          IMAGEM_LOADING2.style.display = "none"
+        })
+      });
+
+
 });
